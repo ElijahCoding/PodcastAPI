90abdf4f7fba43dc6e957f45f417469d380ffae9
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-05 18:20:14 +0300

    endpoints

--- silex/app/Controllers/PodcastController.php
+++ silex/app/Controllers/PodcastController.php
@@ -2,15 +2,34 @@
 
 namespace App\Controllers;
 
+use App\Models\Podcast;
+use Symfony\Component\HttpFoundation\{JsonResponse, Response};
+
 class PodcastController
 {
   public function index()
   {
-    return 'index';
+    $podcasts = Podcast::get();
+
+    return new JsonResponse($podcasts);
   }
 
-  public function show()
+  public function show($id)
   {
-    return 'show';
+    $podcast = Podcast::find($id);
+
+    // if ($podcast === null) {
+    //   return new JsonResponse([
+    //     'error' => [
+    //       'title' => 'Podcast not found.'
+    //     ]
+    //   ], 404);
+    // }
+
+    if ($podcast === null) {
+      return new Response(null, 404);
+    }
+
+    return new JsonResponse($podcast);
   }
 }
