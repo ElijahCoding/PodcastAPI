5f39aafb0b214e58a79c5bf07aaf2a6ee0313eff
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-05 14:53:22 +0300

    eloquent set up

--- silex/bootstrap/app.php
+++ silex/bootstrap/app.php
@@ -2,6 +2,16 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+
+try {
+    $dotenv = new \Dotenv\Dotenv(__DIR__ . '/../');
+    $dotenv->load();
+} catch (\Dotenv\Exception\InvalidPathException $e) {
+    //
+}
+
+require_once __DIR__ . '/database.php';
+
 $app = new Silex\Application([
     'debug' => true
 ]);
