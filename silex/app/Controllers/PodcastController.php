80af41348a21a7887f143e4b2c3ee7a1d7c2f020
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-05 18:41:50 +0300

    pagination api

--- silex/app/Controllers/PodcastController.php
+++ silex/app/Controllers/PodcastController.php
@@ -3,48 +3,45 @@
 namespace App\Controllers;
 
 use App\Models\Podcast;
-use App\Transformers\PodcastTransformer;
 use Symfony\Component\HttpFoundation\{JsonResponse, Response};
+use App\Transformers\PodcastTransformer;
+
 use League\Fractal\{
-  Manager as Fractal,
-  Resource\Item,
-  Resource\Collection
+    Manager as Fractal,
+    Resource\Item,
+    Resource\Collection,
+    Pagination\IlluminatePaginatorAdapter
 };
 
 class PodcastController
 {
-  protected $fractal;
+    protected $fractal;
 
-  public function __construct(Fractal $fractal)
-  {
-    $this->fractal = $fractal;
-  }
-
-  public function index()
-  {
-    $podcasts = Podcast::get();
-
-    return new JsonResponse($podcasts);
-  }
-
-  public function show($id)
-  {
-    $podcast = Podcast::find($id);
-
-    // if ($podcast === null) {
-    //   return new JsonResponse([
-    //     'error' => [
-    //       'title' => 'Podcast not found.'
-    //     ]
-    //   ], 404);
-    // }
-
-    if ($podcast === null) {
-      return new Response(null, 404);
+    public function __construct(Fractal $fractal)
+    {
+        $this->fractal = $fractal;
     }
 
-    $transformer = new Item($podcast, new PodcastTransformer);
+    public function index()
+    {
+        $podcasts = Podcast::latest()->paginate(2);
 
-    return new JsonResponse($this->fractal->createData($transformer)->toArray());
-  }
+        $transformer = (new Collection($podcasts->getCollection(), new PodcastTransformer))
+            ->setPaginator(new IlluminatePaginatorAdapter($podcasts));
+
+        return new JsonResponse($this->fractal->createData($transformer)->toArray());
+    }
+
+    public function show($id)
+    {
+        $podcast = Podcast::find($id);
+
+        if ($podcast === null) {
+            return new Response(null, 404);
+        }
+
+        $transformer = new Item($podcast, new PodcastTransformer);
+
+        return new JsonResponse($this->fractal->createData($transformer)->toArray());
+    }
 }
--- silex/app/Transformers/PodcastTransformer.php
+++ silex/app/Transformers/PodcastTransformer.php
@@ -7,14 +7,15 @@ use League\Fractal\TransformerAbstract;
 
 class PodcastTransformer extends TransformerAbstract
 {
-  public function transform(Podcast $podcast)
-  {
-    return [
-      'id' => $podcast->id,
-      'title' => $podcast->title,
-      'description' => $podcast->description,
-      'created_at' => $podcast->created_at->toDateTimeString(),
-      'created_at_human' => $podcast->created_at->diffForHumans()
-    ];
-  }
+    public function transform(Podcast $podcast)
+    {
+        return [
+            'id' => $podcast->id,
+            'title' => $podcast->title,
+            'description' => $podcast->description,
+            'files' => $podcast->files,
+            'created_at' => $podcast->created_at->toDateTimeString(),
+            'created_at_human' => $podcast->created_at->diffForHumans(),
+        ];
+    }
 }
