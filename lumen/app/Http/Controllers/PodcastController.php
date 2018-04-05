d3e6817847efc947eabc3f31d1329aef5878ad7d
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-05 19:37:44 +0300

    fractal transformer

--- lumen/app/Http/Controllers/PodcastController.php
+++ lumen/app/Http/Controllers/PodcastController.php
@@ -3,20 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Podcast;
+use App\Transformers\PodcastTransformer;
 
 class PodcastController extends Controller
 {
     public function index()
     {
-      $podcasts = Podcast::get();
+      $podcasts = Podcast::paginate(2);
 
-      return $podcasts;
+      return fractal($podcasts, new PodcastTransformer)->toArray();
     }
 
     public function show($id)
     {
       $podcast = Podcast::find($id);
 
-      return $podcast;
+      if ($podcast === null) {
+        return response(null, 404);
+      }
+
+      return fractal($podcast, new PodcastTransformer)->toArray();
     }
 }
