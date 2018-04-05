9bd93de9099674a2e0f843ec61f454c10456befd
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-05 18:44:24 +0300

    middleware

--- silex/bootstrap/app.php
+++ silex/bootstrap/app.php
@@ -1,8 +1,6 @@
 <?php
 
-
 require_once __DIR__ . '/../vendor/autoload.php';
-
 
 try {
     $dotenv = new \Dotenv\Dotenv(__DIR__ . '/../');
@@ -12,19 +10,22 @@ try {
 }
 
 require_once __DIR__ . '/database.php';
+require_once __DIR__ . '/pagination.php';
 
 $app = new Silex\Application([
-    'debug' => true
+    'debug' => getenv('APP_DEBUG')
 ]);
+
+$app->after(new \App\Middleware\Cors());
 
 $app->register(new \Silex\Provider\ServiceControllerServiceProvider());
 
-$app['fractal'] = function ($app) {
-  return new \League\Fractal\Manager();
+$app['fractal'] = function () {
+    return new \League\Fractal\Manager();
 };
 
 $app['podcast.controller'] = function ($app) {
-  return new \App\Controllers\PodcastController($app['fractal']);
+    return new \App\Controllers\PodcastController($app['fractal']);
 };
 
 require_once __DIR__ . '/../routes/api.php';
